43a3fa8060f5d0ecf1765f47dfa7d848496723af
Michael Oldfield <magister_ludi_github@yahoo.com.au>
2023-05-14 06:08:02 +0100

    Added in-place FFT; simplified Chirp-z code.

--- src/chirpz.jl
+++ src/chirpz.jl
@@ -1,35 +1,31 @@
-struct Placeholder{T <: Number}
-    r::T
-    c::T
-end
 
-struct scaleDataType
-    newDim::Placeholder{Int}
-    nTiles::Placeholder{Int}
-    paddedDim::Placeholder{Int}
-    offset::Placeholder{Int}
+struct ScaleData{F <: Function}
+    newDim::NTuple{2, Int}
+    nTiles::NTuple{2, Int}
+    paddedDim::NTuple{2, Int}
+    offset::NTuple{2, Int}
     scale::Float64
-    alpha::Placeholder{Float64}
+    alpha::NTuple{2, Float64}
     isFFT::Bool
-    transform::Function
+    transform::F
 end
 
 scale_chirpz(eb::Eigenbrot, xfactor::Real, yfactor::Real) =
-    scale_y_chirpz(scale_x_chirpz(eb,xfactor), yfactor)
+    scale_y_chirpz(scale_x_chirpz(eb, xfactor), yfactor)
 
 function scale_x_chirpz(eb::Eigenbrot, factor::Real)
     factor == 1 && return copy(eb)
     h, w = size(eb)
-    newDim = Placeholder(h, round(Int, w * factor))
-    sc = scaleDataType(
+    newDim = (h, round(Int, w * factor))
+    sc = ScaleData(
         newDim,                                  # newDim
-        Placeholder(1, ceil(Int, newDim.c / w)), # nTiles
-        Placeholder(h, 2 * w),                   # paddedDim
-        Placeholder(0, w >> 1),                  # offset
+        (1, ceil(Int, newDim[2] / w)),           # nTiles
+        (h, 2 * w),                              # paddedDim
+        (0, w >> 1),                             # offset
         sqrt(2.0),                               # scale
-        Placeholder(0.0, π / (w * factor)),      # alpha
+        (0.0, π / (w * factor)),                 # alpha
         eb.fft,                                  # isFFT
-        fftx                                     # transform
+        fftx!,                                   # transform
     )
     scale_1d(eb, sc)
 end
@@ -37,69 +33,72 @@ end
 function scale_y_chirpz(eb::Eigenbrot, factor::Real)
     factor == 1 && return copy(eb)
     h, w = size(eb)
-    newDim = Placeholder(round(Int, h * factor), w)
-    sc = scaleDataType(
+    newDim = (round(Int, h * factor), w)
+    sc = ScaleData(
         newDim,                                  # newDim
-        Placeholder(ceil(Int, newDim.r / h), 1), # nTiles
-        Placeholder(2 * h, w),                   # paddedDim
-        Placeholder(h >> 1, 0),                  # offset
+        (ceil(Int, newDim[1] / h), 1),           # nTiles
+        (2 * h, w),                              # paddedDim
+        (h >> 1, 0),                             # offset
         sqrt(2.0),                               # scale
-        Placeholder(π / (h * factor), 0.0),      # alpha
+        (π / (h * factor), 0.0),                 # alpha
         eb.fft,                                  # isFFT
-        ffty                                     # transform
+        ffty!,                                   # transform
     )
     scale_1d(eb, sc)
 end
 
-function scale_1d(input::Eigenbrot, sc::scaleDataType)
+function scale_1d(input::Eigenbrot, sc::ScaleData)
     h, w = size(input)
-    result = Eigenbrot(sc.newDim.r, sc.newDim.c, sc.isFFT)
-    quadProd1 = Eigenbrot(sc.paddedDim.r, sc.paddedDim.c, !sc.isFFT)
-    fill!(quadProd1.vals, 0.0)
-    inputFFT = sc.transform(input, true)
+    inputFFT = sc.transform(copy(input), true)
     inputFFT.fft = sc.isFFT
 
-    for cc in 1:w
-        r2x = (div(w, 2) - cc + 1) ^ 2 * sc.alpha.c
-        for rr in 1:h
-            r2y = (div(h, 2) - rr + 1) ^ 2 * sc.alpha.r
-            quadProd1[rr + sc.offset.r, cc + sc.offset.c] =
-                inputFFT[rr, cc] * (cos(r2x + r2y) - im * sin(r2x + r2y))
+    result = Eigenbrot(sc.newDim[1], sc.newDim[2], sc.isFFT)
+    quad1 = Eigenbrot(fill(zero(ComplexF64), sc.paddedDim[1], sc.paddedDim[2]), !sc.isFFT)
+
+    for cc = 1:w
+        r2x = (div(w, 2) - cc + 1)^2 * sc.alpha[2]
+        for rr = 1:h
+            r2y = (div(h, 2) - rr + 1)^2 * sc.alpha[1]
+            quad1[rr + sc.offset[1], cc + sc.offset[2]] = inputFFT[rr, cc] * cis(-r2x - r2y)
         end
     end
-    quadFFT1 = sc.transform(quadProd1, true)
-    quadProd2 = Eigenbrot(sc.paddedDim.r, sc.paddedDim.c, !sc.isFFT)
-    fftProd = Eigenbrot(sc.paddedDim.r, sc.paddedDim.c, sc.isFFT)
-    for deltaHor in 1:sc.nTiles.c
-        firstXpixel = (deltaHor == sc.nTiles.c && mod(sc.newDim.c, w) != 0) ?
-            w - mod(sc.newDim.c, w) + 1 : 1
-        for deltaVer in 1:sc.nTiles.r
-            firstYpixel = (deltaVer == sc.nTiles.r && mod(sc.newDim.r, h) != 0) ?
-                h - mod(sc.newDim.r, h) + 1 : 1
+    sc.transform(quad1, true)
+    quad2 = Eigenbrot(sc.paddedDim[1], sc.paddedDim[2])
+    fftProd = Eigenbrot(sc.paddedDim[1], sc.paddedDim[2])
 
-            for cc in 1:sc.paddedDim.c
-                r2x = (div(sc.paddedDim.c, 2) - cc + 1 + (deltaHor - 1) * w) ^ 2 * sc.alpha.c
-                for rr in 1:sc.paddedDim.r
-                    r2y = (div(sc.paddedDim.r, 2) - rr + 1 + (deltaVer - 1) * h) ^ 2 * sc.alpha.r
-                    quadProd2[rr, cc] = cos(r2x + r2y) + im * sin(r2x + r2y)
+    for deltaHor = 1:(sc.nTiles[2])
+        firstXpixel =
+            (deltaHor == sc.nTiles[2] && mod(sc.newDim[2], w) != 0) ? w - mod(sc.newDim[2], w) + 1 :
+            1
+        for deltaVer = 1:(sc.nTiles[1])
+            firstYpixel =
+                (deltaVer == sc.nTiles[1] && mod(sc.newDim[1], h) != 0) ?
+                h - mod(sc.newDim[1], h) + 1 : 1
+
+            for cc = 1:(sc.paddedDim[2])
+                r2x = (div(sc.paddedDim[2], 2) - cc + 1 + (deltaHor - 1) * w)^2 * sc.alpha[2]
+                for rr = 1:(sc.paddedDim[1])
+                    r2y = (div(sc.paddedDim[1], 2) - rr + 1 + (deltaVer - 1) * h)^2 * sc.alpha[1]
+                    quad2[rr, cc] = cis(r2x + r2y)
                 end
             end
-            quadFFT2 = sc.transform(quadProd2, true)
-            fftProd = quadFFT2 * quadFFT1
-            conv = sc.transform(fftProd, true)
-            for cc in firstXpixel:w
-                r2x = (div(w, 2) - cc + 1 + (deltaHor - 1) * w) ^ 2 * sc.alpha.c
-                col = mod1(cc + div(sc.newDim.c - w, 2) - (deltaHor - 1) * w + sc.newDim.c,
-                           sc.newDim.c)
-                for rr in firstYpixel:h
-                    r2y = (div(h, 2) - rr + 1 + (deltaVer - 1) * h) ^ 2 * sc.alpha.r
-                    row = mod1(rr + div(sc.newDim.r - h, 2) - (deltaVer - 1) * h + sc.newDim.r,
-                               sc.newDim.r)
-                    newval = sc.scale * conv[rr + sc.offset.r, cc + sc.offset.c] *
-                        (cos(r2x + r2y) - im * sin(r2x + r2y))
-                    result[row,col] =
-                        sc.scale * conv[rr + sc.offset.r, cc + sc.offset.c] *
-                        (cos(r2x + r2y) - im * sin(r2x + r2y))
+            quad2.fft = !sc.isFFT
+            fftProd.vals .= sc.transform(sc.transform(quad2, true) * quad1, true)
+
+            for cc = firstXpixel:w
+                r2x = (div(w, 2) - cc + 1 + (deltaHor - 1) * w)^2 * sc.alpha[2]
+                col = mod1(
+                    cc + div(sc.newDim[2] - w, 2) - (deltaHor - 1) * w + sc.newDim[2],
+                    sc.newDim[2],
+                )
+                for rr = firstYpixel:h
+                    r2y = (div(h, 2) - rr + 1 + (deltaVer - 1) * h)^2 * sc.alpha[1]
+                    row = mod1(
+                        rr + div(sc.newDim[1] - h, 2) - (deltaVer - 1) * h + sc.newDim[1],
+                        sc.newDim[1],
+                    )
+                    result[row, col] =
+                        sc.scale * fftProd[rr + sc.offset[1], cc + sc.offset[2]] * cis(-r2x - r2y)
                 end
             end
         end
--- src/fft.jl
+++ src/fft.jl
@@ -2,15 +2,16 @@
 """
     alt_xy(r::Integer, c::Integer)
     alt_xy(rc::Tuple{T, U}) where {T <: Integer, U <: Integer}
+
 Construct a matrix with `r` rows and `c` columns, with elements
 alternating between `+1.0` and `-1.0`.
 The first element is +1.
 """
 function alt_xy(r::Integer, c::Integer)
     a = repeat([1.0, -1.0], outer = div(r + 1, 2))
-    isodd(r) && (a = a[1:end - 1])
+    isodd(r) && (a = a[1:(end - 1)])
     b = repeat(vcat(a, -a), outer = div(c + 1, 2))
-    isodd(c) && (b = b[1:r * c])
+    isodd(c) && (b = b[1:(r * c)])
     reshape(b, r, c)
 end
 
@@ -19,6 +20,7 @@ alt_xy(rc::Tuple{T, U}) where {T <: Integer, U <: Integer} = alt_xy(rc[1], rc[2]
 """
     alt_x(r::Integer, c::Integer)
     alt_x(rc::Tuple{T, U}) where {T <: Integer, U <: Integer}
+
 Construct a matrix with `r` rows and `c` columns, with columns
 alternating between `+1.0` and `-1.0`.
 The first column is +1.
@@ -26,7 +28,7 @@ The first column is +1.
 function alt_x(r::Integer, c::Integer)
     a = repeat([1.0, -1.0], inner = r)
     b = repeat(a, outer = div(c + 1, 2))
-    isodd(c) && (b = b[1:r * c])
+    isodd(c) && (b = b[1:(r * c)])
     reshape(b, r, c)
 end
 
@@ -35,13 +37,14 @@ alt_x(rc::Tuple{T, U}) where {T <: Integer, U <: Integer} = alt_x(rc[1], rc[2])
 """
     alt_y(r::Integer, c::Integer)
     alt_y(rc::Tuple{T, U}) where {T <: Integer, U <: Integer}
+
 Construct a matrix with `r` rows and `c` columns, with rows
 alternating between `+1.0` and `-1.0`.
 The first row is +1.
 """
 function alt_y(r::Integer, c::Integer)
     a = repeat([1.0, -1.0], outer = div(r + 1, 2))
-    isodd(r) && (a = a[1:end-1])
+    isodd(r) && (a = a[1:(end - 1)])
     b = repeat(a, outer = c)
     reshape(b, r, c)
 end
@@ -50,6 +53,7 @@ alt_y(rc::Tuple{T, U}) where {T <: Integer, U <: Integer} = alt_y(rc[1], rc[2])
 
 """
     fft(eb[, recentre = true])
+
 Returns the two-dimensional DFT of Eigenbrot `eb`. If
 `recentre` is `true` the DFT is stored so that the value
 at the coordinate origin is at the central location of
@@ -73,57 +77,64 @@ end
 
 """
     fftx(eb[, recentre = true])
+
 Returns the one-dimensional DFT of Eigenbrot `eb` in
 the `x`-direction. If `recentre` is `true` the DFT is
 stored so that the value at the coordinate origin is
 at the central location of the data.
 """
-function fftx(eb::Eigenbrot, recentre::Bool = true)
-    w = width(eb)
-    h = height(eb)
-    scale = 1.0 / sqrt(w)
+function fftx!(eb::Eigenbrot, recentre::Bool = true)
     if recentre
-        alt = alt_x(h, w)
-        data =  alt .* eb.vals
-        eb.fft ? fft!(data, 2) : bfft!(data, 2)
-        Eigenbrot(scale * (alt .* data), !eb.fft)
+        w = width(eb)
+        h = height(eb)
+        scale = 1.0 / sqrt(h)
+        alt = alt_y(h, w)
+        eb.vals .*= alt
+        eb.fft ? fft!(eb.vals, 2) : bfft!(eb.vals, 2)
+        eb.vals .= scale * (alt .* eb.vals)
     else
-        data = copy(eb.vals)
-        eb.fft ? fft!(data, 2) : bfft!(data, 2)
-        Eigenbrot(scale * data, !eb.fft)
+        eb.fft ? fft!(eb.vals, 2) : bfft!(eb.vals, 2)
     end
+    eb.fft = !eb.fft
+    return eb
 end
+
+fftx(eb::Eigenbrot, recentre::Bool = true) = fftx!(copy(eb), recentre)
 
 """
     ffty(eb[, recentre = true])
+
 Returns the one-dimensional DFT of Eigenbrot `eb` in
 the `y`-direction. If `recentre` is `true` the DFT is
 stored so that the value at the coordinate origin is
 at the central location of the data.
 """
-function ffty(eb::Eigenbrot, recentre::Bool = true)
-    w = width(eb)
-    h = height(eb)
-    scale = 1.0 / sqrt(h)
+function ffty!(eb::Eigenbrot, recentre::Bool = true)
     if recentre
+        w = width(eb)
+        h = height(eb)
+        scale = 1.0 / sqrt(h)
         alt = alt_y(h, w)
-        data = alt .* eb.vals
-        eb.fft ? fft!(data, 1) : bfft!(data, 1)
-        Eigenbrot(scale * (alt .* data), !eb.fft)
+        eb.vals .*= alt
+        eb.fft ? fft!(eb.vals, 1) : bfft!(eb.vals, 1)
+        eb.vals .= scale * (alt .* eb.vals)
     else
-        data = copy(eb.vals)
-        eb.fft ? fft!(data, 1) : bfft!(data, 1)
-        Eigenbrot(scale * data, !eb.fft)
+        eb.fft ? fft!(eb.vals, 1) : bfft!(eb.vals, 1)
     end
+    eb.fft = !eb.fft
+    return eb
 end
+
+ffty(eb::Eigenbrot, recentre::Bool = true) = ffty!(copy(eb), recentre)
 
 """
     removeDC(eb)
+
 Return an Eigenbrot that is the Eigenbrot `eb`
 with the zero-frequency component removed.
 """
 function removeDC(eb::Eigenbrot)
-  r = fft(eb, false)
-  r[1, 1] = 0
-  return fft(r, false)
+    r = fft(eb, false)
+    r[1, 1] = 0
+    return fft(r, false)
 end
